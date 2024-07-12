0a5f8f014d7d22e9ecea60ab29c081779cea6663
nconeymmi <allenquicosa@gmail.com>
2024-07-13 06:48:23 +0800

    added cell creation

---
+++ src/cell.py
@@ -0,0 +1,54 @@
+from graphics import Window, Point, Line
+
+class Cell():
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, x2, y1, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        
+        left = Line(Point(x1, y1), Point(x1, y2))
+        right = Line(Point(x2, y1), Point(x2, y2))
+        top = Line(Point(x1, y2), Point(x2, y2))
+        bottom = Line(Point(x1, y1), Point(x2, y1))   
+        
+        line_color = "black"
+
+        if self.has_left_wall:
+            self._win.draw_line(left, fill_color=line_color)
+
+        if self.has_right_wall:
+            self._win.draw_line(right, fill_color=line_color)
+
+        if self.has_top_wall:
+            self._win.draw_line(top, fill_color=line_color)
+        
+        if self.has_bottom_wall:
+            self._win.draw_line(bottom, fill_color=line_color)
+
+
+    def draw_move(self, to_cell, undo=False):
+        line_color = "gray"
+        if not undo:
+            line_color = "red"
+        
+        x1 = (self._x2-self._x1)/2 + self._x1
+        y1 = (self._y2-self._y1)/2 + self._y1
+        x2 = (to_cell._x2-to_cell._x1)/2 + to_cell._x1
+        y2 = (to_cell._y2-to_cell._y1)/2 + to_cell._y1
+
+        self._win.draw_line(
+            Line(Point(x1, y1), Point(x2, y2)), 
+            fill_color=line_color
+        )
--- src/main.py
+++ src/main.py
@@ -1,11 +1,32 @@
 from graphics import Window, Point, Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    p1 = Point(100, 200)
-    p2 = Point(200, 500)
-    line = Line(p1, p2)
-    win.draw_line(line, "blue")
+
+    c1 = Cell(win)
+    c2 = Cell(win)
+    c3 = Cell(win)
+    c4 = Cell(win)
+
+    c1.has_bottom_wall = False
+    c1.draw(100, 200, 200, 100)
+    
+    c2.has_top_wall = False
+    c2.has_right_wall = False
+    c2.draw(100, 200, 300, 200)
+
+    c3.has_left_wall = False
+    c3.has_bottom_wall = False
+    c3.draw(200, 300, 300, 200)
+
+    c4.has_top_wall = False
+    c4.draw(200, 300, 400, 300)
+
+    c1.draw_move(c2)
+    c2.draw_move(c3)
+    c3.draw_move(c4)
+
     win.wait_for_close()
 
 
